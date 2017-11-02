755c83794b45c6228c91ed97b88136e2050ec391
cooltyn <cooltyn@mail.ru>
2017-11-02 17:54:33 +0300

    0.2
    Add size and time computing

--- src/upload.py
+++ src/upload.py
@@ -1,13 +1,10 @@
-import json, os, datetime
+import json, os, datetime, tempfile, base64
 import vk_api
 import Tkinter as tk
 import tkFileDialog as filedialog
 from pathlib import Path
-import tempfile
-import os
+from time import time
 from Crypto.Cipher import AES
-import base64
-
 
 secret_key = 'keiDMEkIiisDkGEf'
 cipher = AES.new(secret_key,AES.MODE_ECB)
@@ -42,7 +39,7 @@ def input_password():
         json.dump(pair, file)
     return pair
         
-def main():    
+def main():
     pair=False
     need_to_login=True
     need_to_select_album=True
@@ -53,7 +50,7 @@ def main():
     
     if Path(login_file).is_file():
         with open(login_file, 'r') as file:
-            pair=ask_password(json.load(file))            
+            pair=ask_password(json.load(file))
     
     while need_to_login:
         try:
@@ -68,7 +65,9 @@ def main():
             need_to_login=False
     
     vk = vk_session.get_api()
+    upload = vk_api.VkUpload(vk_session)
     albums=vk.photos.getAlbums()
+
     print ('Albums list:')
     for i, album in enumerate(albums['items']):
         print (str(i)+': '+get_album_info(album))    
@@ -84,31 +83,52 @@ def main():
     
     root = tk.Tk()
     root.withdraw()
-    files = list(filedialog.askopenfilenames())
-    total_count=len(files)
-    total_loaded=0    
-    print('%.i files is selected for uploading' %total_count)
-
-    upload = vk_api.VkUpload(vk_session)
-
-    for file in files:
+    files=[]
+    filesize_total=float(0)
+    filesize_loaded=0
+    for file in filedialog.askopenfilenames():
+        filesize=int(os.path.getsize(file))
+        filesize_total+=filesize
+        files.append([
+            file,
+            filesize
+        ])
+    
+    files_total=len(files)
+    files_loaded=0
+    print('%.i files is selected for uploading (%.2f MB)' %(files_total, filesize_total/(1024*1024)))
+    batch_size=1
+    files=[files[i:i+batch_size] for i in xrange(0, len(files), batch_size)]
+    time_start=time()
+    
+    for files_chunk in files:
         try:
+            files_tmp=[]
+            for file in files_chunk:
+                files_tmp.append(file[0])
+                filesize_loaded+=file[1]
             photo = upload.photo(
-                file,
+                files_tmp,
                 album_id=album['id']
             )
         except BaseException as e:
-            print ('Uploading error: '+file)
-            error_files.append(file)
+            error_files.append(files_chunk[0])
+            if batch_size is 1:
+                print ('Uploading error: '+files_chunk[0][0])
+            else:
+                print ('Uploading error')
         else:
-            total_loaded+=1        
-            print('Uploaded %.i/%.i files' %(total_loaded,total_count))
-    
+            files_loaded+=len(photo)
+            time_runned=time()-time_start
+            print('Uploaded %.i/%.i files, %.1f minutes left' %(files_loaded, files_total, (time_runned*filesize_total/filesize_loaded-time_runned)/60))
+
     if len(error_files):
-        print ('Not uploaded %.i/%.i files:' %(total_count-total_loaded,total_count))
-        for file in error_files:            
-            print (os.path.basename(file))
-    
-    raw_input('Done, press Enter to exit ')
-        
+        print ('Not uploaded %.i/%.i files:' %(files_total-files_loaded,files_total))
+        if batch_size is 1:
+            for file in error_files:
+                print (os.path.basename(file[0]))
+   
+    raw_input('Done in %.1f minutes, press Enter to exit ' %((time()-time_start)/60))
+
+
 main()
